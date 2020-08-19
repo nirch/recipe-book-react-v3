ff943402829d29f5fe44f5995fed1a8a379c4343
nirch <nirh2@yahoo.com>
2020-08-19 12:25:52 +0300

    LoginPage showing nice Alert when invalid login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Form, Col, Row, Button } from 'react-bootstrap';
+import { Form, Col, Row, Button, Alert } from 'react-bootstrap';
 import './LoginPage.css'
 
 class LoginPage extends Component {
@@ -9,7 +9,8 @@ class LoginPage extends Component {
         
         this.state = {
             emailInput: "",
-            pwdInput: ""
+            pwdInput: "",
+            showInvalidCredentials: false
         }
 
         this.login = this.login.bind(this);
@@ -29,25 +30,30 @@ class LoginPage extends Component {
 
         } else {
             // If the login is not valid: show an error alert
-            alert("not found")
+            this.setState({
+                showInvalidCredentials: true
+            })
         }
     }
 
     render() {
 
-        const { emailInput, pwdInput } = this.state;
+        const { emailInput, pwdInput, showInvalidCredentials } = this.state;
 
         return (
             <div className="p-login">
                 <h1>Login to Recipe Book</h1>
                 or <a href="#/signup">create a new account</a>
                 <Form>
+                    {showInvalidCredentials ? <Alert variant="danger">
+                        Invalid Credientails! Incorrect email or password
+                    </Alert> : null}
                     <Form.Group as={Row} controlId="formHorizontalEmail">
                         <Form.Label column sm={2}>
                             Email
                         </Form.Label>
                         <Col sm={10}>
-                            <Form.Control type="email" placeholder="Email" value={emailInput} onChange={(e) => this.setState({emailInput: e.target.value})}/>
+                            <Form.Control type="email" placeholder="Email" value={emailInput} onChange={(e) => this.setState({emailInput: e.target.value, showInvalidCredentials: false})}/>
                         </Col>
                     </Form.Group>
 
@@ -56,7 +62,7 @@ class LoginPage extends Component {
                             Password
                         </Form.Label>
                         <Col sm={10}>
-                            <Form.Control type="password" placeholder="Password" value={pwdInput} onChange={(e) => this.setState({pwdInput: e.target.value})}/>
+                            <Form.Control type="password" placeholder="Password" value={pwdInput} onChange={(e) => this.setState({pwdInput: e.target.value, showInvalidCredentials: false})}/>
                         </Col>
                     </Form.Group>
 
