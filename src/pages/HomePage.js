05b2064bf20b8247bd4749432e38eb444c119fae
nirch <nirh2@yahoo.com>
2020-08-19 14:22:56 +0300

    "Design" for Home Page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Jumbotron } from 'react-bootstrap';
 import RecipeNavbar from '../components/RecipeNavbar';
 
 class HomePage extends Component {
 
     constructor(props) {
         super(props);
-        
+
     }
 
     render() {
@@ -14,10 +14,16 @@ class HomePage extends Component {
 
         return (
             <div>
-                <RecipeNavbar activeUser={activeUser} handleLogout={handleLogout}/>
-                <Container>
-                    Home Page
-                </Container>
+                <RecipeNavbar activeUser={activeUser} handleLogout={handleLogout} />
+                <Jumbotron>
+                    <Container>
+                        <h1>Recipe Book</h1>
+                        <p>
+                            Master your recipes
+                      </p>
+                    </Container>
+                </Jumbotron>
+
             </div>
         );
     }
