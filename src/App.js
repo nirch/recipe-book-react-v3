5bec720cd17d7170305e185de88e7b91d4ef962e
nirch <nirh2@yahoo.com>
2020-08-19 09:48:44 +0300

    react router + routes for home, login and recipes pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Switch, Route, HashRouter } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RecipesPage from './pages/RecipesPage';
 
 
 class App extends React.Component {
@@ -8,9 +12,19 @@ class App extends React.Component {
   render() {
 
     return (
-      <div>
-        Recipe Book
-      </div>
+      <HashRouter>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route exact path="/login">
+            <LoginPage />
+          </Route>
+          <Route exact path="/recipes">
+            <RecipesPage />
+          </Route>
+        </Switch>
+      </HashRouter>
     );
   }
 }
---
+++ src/pages/RecipesPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class RecipesPage extends Component {
+    render() {
+        return (
+            <div>
+                Recipes Page
+            </div>
+        );
+    }
+}
+
+export default RecipesPage;
